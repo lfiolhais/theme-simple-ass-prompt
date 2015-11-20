89c3f4261eebcdd3450fdd5c77d25e859605ca46
Luís Fiolhais <luisazenhas.fiolhais@gmail.com>
2015-11-20 16:27:04 +0000

    Updated right prompt and Window Title The window name shows the PWD shortened and the right prompt shows a cross if the previous command fails.

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,3 +1,10 @@
+set __fish_status_prompt_char_failed '✘'
+
 function fish_right_prompt
   set -l code $status
+
+  set_color red
+  if [ $code -eq 127 ]
+    printf '%s' $__fish_status_prompt_char_failed
+  end
 end
---
+++ fish_title.fish
@@ -0,0 +1,4 @@
+function fish_title
+  # Customize the title bar of the terminal window.
+  printf '%s' (prompt_pwd)
+end
